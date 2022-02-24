17858e2a2cfe34eae350e0928c2f275aa9c9c1ea
vo-ngoc <ngoc.vo@virta.global>
2022-02-24 23:08:31 +0200

    Init DB and restructure repo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use rust_backend::run;
+use rust_backend::startup::run;
 use std::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
